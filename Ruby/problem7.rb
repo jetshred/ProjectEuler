54d625e338b594578b8e61f81f71e2bc39429a15
Jon Hester <jetshred@gmail.com>
2010-12-02 18:57:14 -0500

    problem 7 done

--- Ruby/problem7.rb
+++ Ruby/problem7.rb
@@ -1,23 +1,24 @@
 #However, rather than testing all m up to n − 1, it is only necessary to test m up to sqrt(n) 
 #if n is composite then it can be factored into two values, at least one of which must be less than or equal to sqrt(n).
-x = 1
-n = 1
+x = 3
+n = 3
 
-while n < 6
-  puts n
-  test = 1
+while n <= 10001
+  test = 3
   if x % 2 != 0
     sqrtx = x**(0.5)
-    while x % test != 0 and test < sqrtx
-        test = test + 2
+    sqrtx = sqrtx.to_i + 1
+    while x % test != 0 and test <= sqrtx
+        if test == sqrtx
+          n = n + 1
+          puts
+          puts x
+        end
+        test = test + 1
     end
   end
   x = x + 1
-  n = n + 1
 end
 
-puts x
-
-puts 9**(0.5) % 3
 
  
